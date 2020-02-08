7fd18118c53a679175394be1cc9cf5c18916d07a
Youmna Srour <youmna.srour@gmail.com>
2020-02-08 15:46:49 +0000

    activities

--- lib/activity.dart
+++ lib/activity.dart
@@ -1,21 +1,5 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
-
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Activities Suggester',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: MyActivityPage(title: 'Activities Suggester'),
-    );
-  }
-}
-
 Map<int, Color> color = {
   50: Color.fromRGBO(136, 14, 79, .1),
   100: Color.fromRGBO(136, 14, 79, .2),
@@ -29,57 +13,23 @@ Map<int, Color> color = {
   900: Color.fromRGBO(136, 14, 79, 1),
 };
 
-class MyActivityPage extends StatefulWidget {
-  MyActivityPage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  _MyActivityPageState createState() => _MyActivityPageState();
-}
-
-class _MyActivityPageState extends State<MyActivityPage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+class ActivityPage extends StatelessWidget {
+  ActivityPage() : super();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title),
+        title: Text("activities"),
       ),
       body: Column(
         children: <Widget>[
           new ActivityBlock(
-              title: 'Reading', color: MaterialColor(0xFFBA55D3, color)),
+              title: 'Cooking', color: MaterialColor(0xFFFF7F50, color)),
           new ActivityBlock(
-              title: 'Cooking', color: MaterialColor(0xFFFF7F50, color))
+              title: 'Reading', color: MaterialColor(0xFFBA55D3, color)),
         ],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
@@ -111,13 +61,11 @@ class ActivityBlock extends StatelessWidget {
       ),
     );
   }
-
-  // Center is a layout widget. It takes a single child and positions it
-  // in the middle of the parent.
-  // reading
-  // arts and crafts
-  // socializing
-  // exersise
-  // reading
-
 }
+// Center is a layout widget. It takes a single child and positions it
+// in the middle of the parent.
+// reading
+// arts and crafts
+// socializing
+// exersise
+// reading
