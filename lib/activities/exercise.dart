305c1ce865209e7e2a3e4172e010b92dd37a6f98
Youmna Srour <youmna.srour@gmail.com>
2020-02-09 13:12:34 +0000

    added audio

--- lib/activities/exercise.dart
+++ lib/activities/exercise.dart
@@ -1,3 +1,4 @@
+import 'package:dementia_hack/components/panel.dart';
 import 'package:flutter/material.dart';
 
 class ExercisePage extends StatelessWidget {
@@ -10,5 +11,51 @@ class ExercisePage extends StatelessWidget {
         appBar: AppBar(
       title: Text("Exercises"),
     ));
+       body: SingleChildScrollView(
+          child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: <Widget>[
+            Align(
+                alignment: Alignment.topLeft,
+                child: Panel(
+                  title: 'Add memories',
+                  height: 160,
+                  icon: Icons.add_a_photo,
+                  description: 'Add a memory for today',
+                  color: Colors.purple,
+                  clickHandler: () =>
+                      moveScreen(ctxt, () => MemoriesInitialPage()),
+                )),
+            Align(
+                alignment: Alignment.topRight,
+                child: Panel(
+                  title: 'Calendar',
+                  height: 160,
+                  icon: Icons.calendar_today,
+                  description: 'View your moods over time',
+                  color: Colors.blue,
+                  clickHandler: () => moveScreen(ctxt, () => CalendarPage()),
+                )),
+            Align(
+                alignment: Alignment.bottomLeft,
+                child: Panel(
+                  title: 'Rewind',
+                  height: 160,
+                  icon: Icons.question_answer,
+                  description: 'Answer questions on your past',
+                  color: Colors.deepOrange,
+                ))
+            Align(
+                alignment: Alignment.bottomRight,
+                child: Panel(
+                  title: 'Activities',
+                  height: 160,
+                  icon: Icons.directions_run,
+                  description: 'Choose an activity to do today!',
+                  color: Colors.green,
+                  clickHandler: () => moveScreen(ctxt, () => ActivityPage()),
+                )),
+          ],
+        )));
   }
 }
--- lib/components/addAudio.dart
+++ lib/components/addAudio.dart
@@ -1,9 +1,12 @@
 import 'dart:async';
-
+import 'dart:io';
+import 'package:path_provider/path_provider.dart';
+import 'package:uuid/uuid.dart';
 import 'package:dementia_hack/quiz.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart' show DateFormat;
 import 'package:intl/date_symbol_data_local.dart';
+import 'package:path/path.dart' as path;
 
 import 'dart:async';
 import 'package:flutter_sound/flutter_sound.dart';
@@ -16,6 +19,7 @@ class AudioRecorder extends StatefulWidget {
 class _AudioRecorderState extends State<AudioRecorder> {
   bool _isRecording = false;
   bool _isPlaying = false;
+  String _path;
   StreamSubscription _recorderSubscription;
   StreamSubscription _dbPeakSubscription;
   StreamSubscription _playerSubscription;
@@ -43,7 +47,13 @@ class _AudioRecorderState extends State<AudioRecorder> {
       String path = await flutterSound.startRecorder();
       print('startRecorder: $path');
 
+      this.setState(() {
+        this._path = path;
+      });
+
       _recorderSubscription = flutterSound.onRecorderStateChanged.listen((e) {
+        print(e);
+        if (e == null) return;
         DateTime date = new DateTime.fromMillisecondsSinceEpoch(
             e.currentPosition.toInt(),
             isUtc: true);
@@ -56,7 +66,7 @@ class _AudioRecorderState extends State<AudioRecorder> {
       _dbPeakSubscription =
           flutterSound.onRecorderDbPeakChanged.listen((value) {
         print("got update -> $value");
-        setState(() {
+        this.setState(() {
           this._dbLevel = value;
         });
       });
@@ -73,6 +83,13 @@ class _AudioRecorderState extends State<AudioRecorder> {
     try {
       String result = await flutterSound.stopRecorder();
       print('stopRecorder: $result');
+
+      final String newPath = (await getApplicationDocumentsDirectory()).path;
+      final String newFileName = Uuid().v4() + path.extension(this._path);
+      final File copiedAudio =
+          await new File(this._path).copy('$newPath/$newFileName');
+
+      print(copiedAudio.path);
 
       if (_recorderSubscription != null) {
         _recorderSubscription.cancel();
@@ -92,6 +109,7 @@ class _AudioRecorderState extends State<AudioRecorder> {
   }
 
   void startPlayer() async {
+    if (this._isPlaying) return;
     String path = await flutterSound.startPlayer(null);
     await flutterSound.setVolume(1.0);
     print('startPlayer: $path');
@@ -135,8 +153,13 @@ class _AudioRecorderState extends State<AudioRecorder> {
   }
 
   void pausePlayer() async {
+    if (!this._isPlaying) return;
+
     String result = await flutterSound.pausePlayer();
     print('pausePlayer: $result');
+    this.setState(() {
+      this._isPlaying = false;
+    });
   }
 
   void resumePlayer() async {
@@ -151,19 +174,44 @@ class _AudioRecorderState extends State<AudioRecorder> {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        theme: ThemeData(primarySwatch: Colors.pink),
-        home: Scaffold(
-            appBar: AppBar(
-              centerTitle: true,
-              title: const Text('Audio Recorder'),
-            ),
-            body: Row(children: <Widget>[
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text('Audio Recorder'),
+      ),
+      body: ListView(
+        children: <Widget>[
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Container(
+                margin: EdgeInsets.only(top: 24.0, bottom: 16.0),
+                child: Text(
+                  this._recorderTxt,
+                  style: TextStyle(
+                    fontSize: 48.0,
+                    color: Colors.black,
+                  ),
+                ),
+              ),
+              _isRecording
+                  ? LinearProgressIndicator(
+                      value: 100.0 / 160.0 * (this._dbLevel ?? 1) / 100,
+                      valueColor: AlwaysStoppedAnimation<Color>(Colors.green),
+                      backgroundColor: Colors.red,
+                    )
+                  : Container()
+            ],
+          ),
+          Row(
+            children: <Widget>[
               Container(
                 width: 56.0,
                 height: 56.0,
                 margin: EdgeInsets.all(10.0),
                 child: FloatingActionButton(
+                  heroTag: 'record',
                   onPressed: () {
                     if (!this._isRecording) {
                       return this.startRecorder();
@@ -173,6 +221,80 @@ class _AudioRecorderState extends State<AudioRecorder> {
                   child: this._isRecording ? Icon(Icons.stop) : Icon(Icons.mic),
                 ),
               ),
-            ])));
+            ],
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+          ),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Container(
+                margin: EdgeInsets.only(top: 60.0, bottom: 16.0),
+                child: Text(
+                  this._playerTxt,
+                  style: TextStyle(
+                    fontSize: 48.0,
+                    color: Colors.black,
+                  ),
+                ),
+              ),
+            ],
+          ),
+          Row(
+            children: <Widget>[
+              Container(
+                width: 56.0,
+                height: 56.0,
+                margin: EdgeInsets.all(8.0),
+                child: FloatingActionButton(
+                  heroTag: 'play',
+                  onPressed: () {
+                    startPlayer();
+                  },
+                  child: Icon(Icons.play_arrow),
+                ),
+              ),
+              Container(
+                width: 56.0,
+                height: 56.0,
+                margin: EdgeInsets.all(8.0),
+                child: FloatingActionButton(
+                  heroTag: 'pause',
+                  onPressed: () {
+                    pausePlayer();
+                  },
+                  child: Icon(Icons.pause),
+                ),
+              ),
+              Container(
+                width: 56.0,
+                height: 56.0,
+                margin: EdgeInsets.all(8.0),
+                child: FloatingActionButton(
+                  heroTag: 'stop',
+                  onPressed: () {
+                    stopPlayer();
+                  },
+                  child: Icon(Icons.stop),
+                ),
+              ),
+            ],
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+          ),
+          Container(
+              height: 56.0,
+              child: Slider(
+                  value: slider_current_position,
+                  min: 0.0,
+                  max: max_duration,
+                  onChanged: (double value) async {
+                    await flutterSound.seekToPlayer(value.toInt());
+                  },
+                  divisions: max_duration.toInt()))
+        ],
+      ),
+    );
   }
 }
