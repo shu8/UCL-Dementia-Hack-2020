e5eba3b709c9c73b7dc77c1fbc8fcdb1f20e581d
Nishan Sen <nishan.sen.19@ucl.ac.uk>
2020-02-09 00:32:02 +0000

    Added events for Calendar

--- lib/calendar.dart
+++ lib/calendar.dart
@@ -1,6 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:table_calendar/table_calendar.dart';
 
+import 'Memory.dart';
+
+//create array of memory objects
+
+Memory test1 = Memory(text:"test", date:"1234", media:[]);
+Memory test2 = Memory(text:"test2", date:"1243", media:[]);
+
+var ok = ["ok", "cool", "cool"];
+
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
@@ -35,16 +44,18 @@ class _CalendarPageState extends State<CalendarPage> {
     Widget build(BuildContext context) {
       return Scaffold(
         appBar: AppBar(
+          backgroundColor: Colors.lightBlueAccent,
           title: Text('Flutter Calendar'),
         ),
-        body: SingleChildScrollView(
+        body: SingleChildScrollView(//scroll
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: <Widget>[
-              TableCalendar(
+              TableCalendar( //calendar settings
+                initialCalendarFormat: CalendarFormat.month,
                 calendarStyle: CalendarStyle(
-                  todayColor: Colors.orangeAccent,
-                  selectedColor: Theme.of(context).primaryColor,
+                  todayColor: Colors.pink,
+                  selectedColor: Colors.lightBlueAccent,
                   todayStyle: TextStyle(
                     fontWeight: FontWeight.bold,
                     fontSize: 18.0,
@@ -52,8 +63,58 @@ class _CalendarPageState extends State<CalendarPage> {
                   ),
                   highlightToday: true
                 ),
-                calendarController: _controller,
-              )
+                headerStyle: HeaderStyle( //header (date stuff)
+                  centerHeaderTitle: true,
+                  formatButtonDecoration: BoxDecoration(
+                    color: Colors.pink,
+                    borderRadius: BorderRadius.circular(20.0),
+                  ),
+                  formatButtonTextStyle: TextStyle(
+                    color: Colors.white
+                  ),
+                  formatButtonShowsNext: false,
+                ),
+                //config
+                startingDayOfWeek: StartingDayOfWeek.monday,
+                //builders
+                builders: CalendarBuilders(
+                  //change the selected day look
+                  todayDayBuilder: (context, date, events) => 
+                  Container(
+                    alignment: Alignment.center,
+                    margin: const EdgeInsets.all(5),
+                    decoration: BoxDecoration(
+                      color: Colors.pinkAccent,
+                      borderRadius: BorderRadius.circular(10.0)
+                    ),
+                    child: Text(
+                      date.day.toString(), 
+                      style: TextStyle(
+                        color: Colors.white
+                    ))
+                  ),
+
+                  selectedDayBuilder: (context, date, events) => 
+                  Container(
+                    alignment: Alignment.center,
+                    margin: const EdgeInsets.all(5),
+                    decoration: BoxDecoration(
+                      color: Colors.lightBlueAccent,
+                      borderRadius: BorderRadius.circular(10.0)
+                    ),
+                    child: Text(
+                      date.day.toString(), 
+                      style: TextStyle(
+                        color: Colors.white
+                    ))
+                  )
+                ),
+
+                calendarController: _controller, //actual calendar
+              ),
+              ...ok.map((text) => ListTile(
+                title: Text(text),
+              ))
             ],
           ),
         ),
